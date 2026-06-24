d8c258a8abce6e8ff791fd39f74da593fbe84ead
firsmine <sitishafira.fia@gmail.com>
2026-06-25 04:10:31 +0800

    10

--- DigitalSignature/Signature.py
+++ DigitalSignature/Signature.py
@@ -1,6 +1,11 @@
 from cryptography.hazmat.primitives.asymmetric import rsa, padding
 from cryptography.hazmat.primitives import serialization, hashes
-import os
+import os, hashlib
+
+def get_hash(message):
+  return hashlib.sha256(
+    message.encode()
+  ).hexdigest()
 
 while True:
   print("\n"+"#="*3 + " Digital Signature " + "+#"*3)
@@ -8,7 +13,8 @@ while True:
   print("2. Verify Message")
   print("3. Modify Message")
   print("4. View Message")
-  print("5. Exit")
+  print("5. View Hash")
+  print("6. Exit")
 
   try:
     menu = int(input("Select Menu (1-5): "))
@@ -52,7 +58,7 @@ while True:
     signature = private_key.sign(
       message.encode(),
       padding.PKCS1v15(),
-      hashes.SHA3_256()
+      hashes.SHA256()
     )
     # save signature
     with open("signature.bin", "wb") as file:
@@ -60,7 +66,7 @@ while True:
     
     print("\nMessage Signed Successfully!")
     
-    if input("\nTry another menu? (y/n)").lower() != "y":
+    if input("\nTry another menu? (y/n): ").lower() != "y":
       print(">>> Exit the program.\n")
       break
     
@@ -77,7 +83,6 @@ while True:
       if not os.path.exists(file):
         print(f"{file} not found.")
         missing = True
-    
     if missing:
       continue
     
@@ -103,7 +108,7 @@ while True:
       print("\nINVALID")
       print("Message has been modified")
     
-    if input("\nTry another menu? (y/n)").lower() != "y":
+    if input("\nTry another menu? (y/n): ").lower() != "y":
       print(">>> Exit the program.\n")
       break
 
@@ -120,7 +125,7 @@ while True:
 
     print("\nMessage Modified.")
     
-    if input("\nTry another menu? (y/n)").lower() != "y":
+    if input("\nTry another menu? (y/n): ").lower() != "y":
       print(">>> Exit the program.\n")
       break
   
@@ -133,10 +138,42 @@ while True:
     with open("message.txt", "r") as file:
       print("\nCurrent Message: ")
       print(file.read())
+    if input("\nTry another menu? (y/n): ").lower() != "y":
+      print(">>> Exit the program.\n")
+      break
+
+  # view hash
+  elif menu == 5:
+    if not os.path.exists("message.txt"):
+      print("\nNo message found.")
+      continue
+
+    with open("message.txt", "r") as file:
+      message = file.read()
+
+    hash_value = get_hash(message)
+
+    print("\nCurrent message: ")
+    print(message)
+
+    print("\nSHA256 Hash: ")
+    print(hash_value)
+
+    if input("\nTry another menu? (y/n): ").lower() != "y":
+      print(">>> Exit the program.\n")
+      break
 
   # exit
-  elif menu == 5:
+  elif menu == 6:
     print("\nExit the program\n")
     break
   else:
     print("Invalid menu.")
+    
+"""
+Notes:
+- hash        --> check the integrity
+- signature   --> check the authenticity
+- private key --> make signature
+- public key  --> verify the signature
+"""
