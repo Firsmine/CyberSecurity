28e2defb1e492d62f69cb5df8c1b154f987fd1f8
firsmine <sitishafira.fia@gmail.com>
2026-06-26 03:35:14 +0800

    12

--- Securing Data/PasswordLeakChecker.py
+++ Securing Data/PasswordLeakChecker.py
@@ -9,13 +9,44 @@ leaked_pass = [
 ]
 
 while True:
-  password = input("\nCheck Password: ").lower
+  print("\n"+"#="*3 + " Password Leak Checker " + "=#"*3)
+  print("1. Check Password")
+  print("2. View Database")
+  print("3. Add Leaked Password")
+  print("4. Exit")
   
-  if password in leaked_pass:
-    print("WARNING!\nPassword found in the leaked database.")
+  try:
+    menu = int(input("Select menu (1-4): "))
+  except ValueError:
+    print(">>> Please select a valid number of the menu.")
+    continue
+  
+  if menu == 1:
+    print("\n"+"#="*3 + " Check Password " + "=#"*3)
+    
+    password = input("\nPassword: ").lower
+    
+    if password in leaked_pass:
+      print("WARNING!\nPassword found in the leaked database.")
+    else:
+      print("SAFE.\nPassword not found in the leaked database.")
+    
+    if input("\nTry another menu? (y/n: )").lower() != "y":
+      print("\nExit the program\n")
+      break
+    
+  elif menu == 2:
+    print("\n"+"#="*3 + " Leaked Password Database " + "=#"*3)
+    
+    for i, password in enumerate(
+      leaked_pass,
+      start=1
+    ):
+      print(f"{i}. {password}")
+    
+    if input("\nTry another menu? (y/n: )").lower() != "y":
+      print("\nExit the program\n")
+      break
+    
   else:
-    print("Password not found in the leaked database.")
-  
-  if input("\nCheck another password? (y/n: )").lower() != "y":
-    print("\nExit the program\n")
-    break
+    print("\n>>> Please select a valid number of menu.")
