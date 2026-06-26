4c7b19db65b3fdc2cff230975bdea450e897f58d
firsmine <sitishafira.fia@gmail.com>
2026-06-27 02:26:27 +0800

    13

--- Securing_System/PortScanner.py
+++ Securing_System/PortScanner.py
@@ -1,22 +1,59 @@
 import socket                   # network library
+import time
 
-host = "127.0.0.1"              # scan target port (localhost)
-
-print(f"\nScanning {host}...\n")
-
-for port in range(1, 101):
-  sock = socket.socket(         # made a call to port
-    socket.AF_INET,             # using IPv4
-    socket.SOCK_STREAM          # using TCP 
-  )
-  sock.settimeout(0.1)
+while True:
+  print("\n" + "#="*3 + " Port Scanner " + "=#"*3)
   
-  result = sock.connect_ex(     # return number with connect_ex(), not connect()
-    (host, port)                # if succeed, result 0, else result != 0
-  )
-  
-  if result == 0:
-    print(f"Port {port} : OPEN")
+  print("Choose Target IP Address")
+  print("1. Localhost")
+  print("2. Custom IP address")
+  print("3. Exit")
+  try:
+    target = int(input("\nTarget IP (1-3): "))
     
-  sock.close()
-print("\nScan Finished.")
+    if target == 1:
+      host = "127.0.0.1"              # scan target port (localhost)
+    elif target == 2:
+      host = input("IP Address: ")
+    elif target == 3:
+      print("\nExit the program.\n")
+      break
+    else:
+      print(">>> Please choose the available menu.")
+      continue
+  except ValueError:
+    print(">>> Please input a valid number of the menu")
+    continue
+    
+  try:
+    start = int(input("Start Port: "))
+    end = int(input("End Port: "))
+  except ValueError:
+    print(">>> Please input a valid number of port")
+  
+  if input(f"Scan port {host} from port {start} until port {end}? (y/n): ").lower() != "y":
+    print("\nScan Canceled.\n")
+    break
+
+  print(f"\nScanning {host}...\n")
+
+  for port in range(start, end+1):
+    sock = socket.socket(         # made a call to port
+      socket.AF_INET,             # using IPv4
+      socket.SOCK_STREAM          # using TCP 
+    )
+    sock.settimeout(0.1)
+    
+    result = sock.connect_ex(     # return number with connect_ex(), not connect()
+      (host, port)                # if succeed, result 0, else result != 0
+    )
+    
+    if result == 0:
+      print(f"Port {port} : OPEN")
+      
+    sock.close()
+  print("\nScan Finished.")
+  
+  if input("Scan Port Again? (y/n): ").lower() != "y":
+    print("\nExit the program\n")
+    break
