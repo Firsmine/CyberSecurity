0ee2f1ef76ed9cf10f71009e55239a9f11a3ab36
firsmine <sitishafira.fia@gmail.com>
2026-06-27 03:06:34 +0800

    13

--- Securing_System/PortScanner.py
+++ Securing_System/PortScanner.py
@@ -1,20 +1,28 @@
 import socket                   # network library
-import time
+import time, ipaddress
 
 while True:
-  print("\n" + "#="*3 + " Port Scanner " + "=#"*3)
+  print("\n" + "#="*8 + " Port Scanner " + "=#"*8)
   
+  # menu
   print("Choose Target IP Address")
   print("1. Localhost")
-  print("2. Custom IP address")
+  print("2. Custom IP Address")
   print("3. Exit")
+  
+  # target
   try:
     target = int(input("\nTarget IP (1-3): "))
     
     if target == 1:
       host = "127.0.0.1"              # scan target port (localhost)
     elif target == 2:
-      host = input("IP Address: ")
+      try:
+        host = input("IP Address: ")
+        ipaddress.ip_address(host)
+      except ValueError:
+        print(">>> Input a valid IP address. Example: 192.168.1.1")
+        continue
     elif target == 3:
       print("\nExit the program.\n")
       break
@@ -25,18 +33,25 @@ while True:
     print(">>> Please input a valid number of the menu")
     continue
     
+  # range port
   try:
     start = int(input("Start Port: "))
     end = int(input("End Port: "))
   except ValueError:
     print(">>> Please input a valid number of port")
+    continue
   
+  # confirm action
   if input(f"Scan port {host} from port {start} until port {end}? (y/n): ").lower() != "y":
     print("\nScan Canceled.\n")
     break
 
+  
+  # start scanning
   print(f"\nScanning {host}...\n")
 
+  # logic scan port
+  start_time = time.time()
   for port in range(start, end+1):
     sock = socket.socket(         # made a call to port
       socket.AF_INET,             # using IPv4
@@ -56,8 +71,12 @@ while True:
       print(f"Port {port:<5} : OPEN ({service})")
       
     sock.close()
-  print("\nScan Finished.")
+  # timer
+  end_time = time.time()
+  duration = end_time - start_time
+  print(f"\nScan Finished in {duration:.2f} seconds.")
   
+  # end program
   if input("Scan Port Again? (y/n): ").lower() != "y":
     print("\nExit the program\n")
     break
